70ce5f8fc6580c3d240dfc866cf6485958982984
Zeeshan <gecezeeshan@gmail.com>
2025-12-18 13:51:58 +0400

    fix ent

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
 import Card from '../components/Card'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
